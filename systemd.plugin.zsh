d5d47eb127f63d702e8e3522298a409fc7306233
Mubashshir <ahmubashshir@gmail.com>
2020-11-01 12:13:23 +0600

    patches:
     * systemctl shouldn't be executed with uid 0.
     * alias sc(|u)-*-now in loop.

--- systemd.plugin.zsh
+++ systemd.plugin.zsh
@@ -8,15 +8,13 @@ sudo_commands=(
   link load cancel set-environment unset-environment
   edit)
 
-for c in $user_commands; do; alias sc-$c="systemctl $c"; done
-for c in $sudo_commands; do; alias sc-$c="sudo systemctl $c"; done
-for c in $user_commands; do; alias scu-$c="systemctl --user $c"; done
-for c in $sudo_commands; do; alias scu-$c="systemctl --user $c"; done
+now_commands=(enable disable mask)
 
-alias sc-enable-now="sc-enable --now"
-alias sc-disable-now="sc-disable --now"
-alias sc-mask-now="sc-mask --now"
-
-alias scu-enable-now="scu-enable --now"
-alias scu-disable-now="scu-disable --now"
-alias scu-mask-now="scu-mask --now"
+for c in $user_commands $sudo_commands; do
+	alias sc-$c="systemctl $c"
+	alias scu-$c="systemctl --user $c"
+done
+for c in $now_commands;do
+  alias sc-$c-now="sc-$c --now"
+  alias scu-$c-now="scu-$c --now"
+done
